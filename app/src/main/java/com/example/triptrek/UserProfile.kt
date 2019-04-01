609680687413a899ab21bd9dea1da15dcf96bdf2
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2019-04-01 22:17:43 +0600

    Add user profile edit screen

--- app/src/main/java/com/example/triptrek/UserProfile.kt
+++ app/src/main/java/com/example/triptrek/UserProfile.kt
@@ -1,12 +1,21 @@
 package com.example.triptrek
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_user_profile.*
 
 class UserProfile : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+
+
+        editProfile.setOnClickListener {
+            val intent = Intent(this, UserProfileEdit::class.java)
+            startActivity(intent)
+        }
     }
 }
