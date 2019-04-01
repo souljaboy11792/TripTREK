0607924f271d78c5a0ab0c3c16297d435d07b44e
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2019-04-01 16:39:38 +0600

    Add a simple sign in function

--- app/src/main/java/com/example/triptrek/Login.kt
+++ app/src/main/java/com/example/triptrek/Login.kt
@@ -16,6 +16,10 @@ class Login : AppCompatActivity() {
         SignUpButton.setOnClickListener {
             registerUser()
         }
+
+        SignInButton.setOnClickListener {
+            signinUser()
+        }
     }
 
     private fun registerUser()
@@ -43,6 +47,36 @@ class Login : AppCompatActivity() {
                     .addOnFailureListener {
                         Toast.makeText(this, "User sign up failed. ${it.message}", Toast.LENGTH_SHORT).show()
                         d("TT", "User sign up failed. ${it.message}")
+                        return@addOnFailureListener
+                    }
+        }
+    }
+
+    private fun signinUser()
+    {
+        val user = username.text.toString()
+        val email = email.text.toString()
+        val password = password.text.toString()
+
+        if ( email.isEmpty() || password.isEmpty() )
+        {
+            Toast.makeText(this, "Please enter at least email address and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+        else
+        {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if ( it.isSuccessful )
+                        {
+                            Toast.makeText(this, "User sign in successful", Toast.LENGTH_SHORT).show()
+                            d("TT", "User signed in with email: $email, password: $password and user id: ${it.result.user.uid}")
+                            return@addOnCompleteListener
+                        }
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, "User sign in failed. ${it.message}", Toast.LENGTH_SHORT).show()
+                        d("TT", "User sign in failed. ${it.message}")
                         return@addOnFailureListener
                     }
         }
