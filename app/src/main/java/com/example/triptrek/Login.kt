256c383f92218618771eb4fdb40d093435d4d9b7
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2019-04-01 16:23:16 +0600

    Add a simple sign up function

--- app/src/main/java/com/example/triptrek/Login.kt
+++ app/src/main/java/com/example/triptrek/Login.kt
@@ -3,6 +3,8 @@ package com.example.triptrek
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
@@ -11,12 +13,38 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        SignInButton.setOnClickListener {
-            val email = email.text
-            val username = username.text
-            val password = password.text
+        SignUpButton.setOnClickListener {
+            registerUser()
+        }
+    }
 
-            d("TT", "Username: $username, email: $email password: $password")
+    private fun registerUser()
+    {
+        val user = username.text.toString()
+        val email = email.text.toString()
+        val password = password.text.toString()
+
+        if ( email.isEmpty() || password.isEmpty() )
+        {
+            Toast.makeText(this, "Please enter at least email address and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+        else
+        {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if ( it.isSuccessful )
+                        {
+                            Toast.makeText(this, "User sign up successful", Toast.LENGTH_SHORT).show()
+                            d("TT", "User created with email: $email, password: $password and user id: ${it.result.user.uid}")
+                            return@addOnCompleteListener
+                        }
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, "User sign up failed. ${it.message}", Toast.LENGTH_SHORT).show()
+                        d("TT", "User sign up failed. ${it.message}")
+                        return@addOnFailureListener
+                    }
         }
     }
 
